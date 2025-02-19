159cfe12351df29969ea76ba9d9ac227a3a65366
Hazem Krimi <hazem.krimi@finteum.com>
2025-02-19 18:54:04 +0100

    Remap accepting copilot suggestions

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -41,6 +41,13 @@ return {
     config = function()
       vim.keymap.set('n', '<leader>ec', ':Copilot enable<CR>', { desc = 'Enable Copilot', noremap = false, silent = true })
       vim.keymap.set('n', '<leader>dc', ':Copilot disable<CR>', { desc = 'Disable Copilot', noremap = false, silent = true })
+      vim.keymap.set('i', '<C-g>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+        silent = true,
+        noremap = true,
+      })
+      vim.g.copilot_no_tab_map = true
     end,
   },
 }
