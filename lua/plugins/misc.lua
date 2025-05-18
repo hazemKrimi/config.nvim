59f4e96d11eddc1511a819a753148b930ebcddac
Hazem Krimi <me@hazemkrimi.tech>
2025-05-19 00:12:20 +0100

    Remove the correct plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -22,11 +22,11 @@ return {
     opts = {},
   },
   {
-    -- High-performance color highlighter
-    'norcalli/nvim-colorizer.lua',
-    config = function()
-      require('colorizer').setup()
-    end,
+    -- Highlight todo, notes, etc in comments
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
   },
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   {
