abb6181701e25576fdfe00323840aba2102e2225
Hazem Krimi <me@hazemkrimi.tech>
2026-04-03 13:06:54 +0100

    feat: treesitter folding config autocommand

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -57,3 +57,16 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     end
   end,
 })
+
+-- Folding config
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    if require("nvim-treesitter.parsers").has_parser() then
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    else
+      vim.opt.foldmethod = "syntax"
+    end
+  end,
+})
